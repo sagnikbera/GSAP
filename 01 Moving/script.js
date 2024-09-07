527029dc7001230c82337b143b094315827cf69c
sagnikbera <sagnikberaofficial@gmail.com>
2024-09-07 07:32:35 +0530

    update

---
+++ 01 Moving/script.js
@@ -0,0 +1,15 @@
+// gsap.to("#box1",{
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1,
+// })
+
+gsap.from("#box1",{
+    x:1000,
+    rotate:360,
+    backgroundColor:"blue",
+    duration:2,
+    delay:1,
+})
---
+++ 02 Timeline/script.js
@@ -0,0 +1,25 @@
+ let tl = gsap.timeline();
+ 
+ tl.to("#box1",{
+    x:1200,
+    rotate:360,
+   //  delay:1.5,
+    scale:0.5,
+    duration:1,
+ })
+
+tl.to("#box2",{
+    x:1200,
+    rotate:360,
+   //  delay:3,
+    scale:0.5,
+    duration:1,
+ })
+
+ tl.to("#box3",{
+   x:1200,
+   rotate:360,
+  //  delay:4.5,
+   scale:0.5,
+   duration:1,
+})
